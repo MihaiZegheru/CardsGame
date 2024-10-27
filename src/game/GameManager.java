067596eaf1d9f3c2ccfbb150949f27d2da493660
MihaiZegheru <mihai.z.2005@gmail.com>
2024-10-27 12:35:38 +0200

    Created base for GameManager singleton.
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -1,4 +1,30 @@
 package game;
 
+import fileio.Input;
+
 public class GameManager {
+    private static GameManager instance = null;
+    public static GameManager GetInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    private Input descriptor;
+
+    // TODO: Return a game-end object.
+    // Hosts the main loop of the game with a game descriptor.
+    public void Start(Input descriptor) {
+        if (descriptor == null) {
+            System.out.println("Descriptor not loaded.");
+            System.exit(-1);
+        }
+        this.descriptor = descriptor;
+
+        // Game lop goes here
+
+        // Set null after every game.
+        descriptor = null;
+    }
 }
