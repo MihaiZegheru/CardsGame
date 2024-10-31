e449d6956edecc8d4659b52c489179dcf97f0f69
MihaiZegheru <mihai.z.2005@gmail.com>
2024-10-31 23:40:46 +0200

    Added base casting logic.
    Signed-off-by: MihaiZegheru <mihai.z.2005@gmail.com>

--- src/game/CasterMinion.java
+++ src/game/CasterMinion.java
@@ -2,6 +2,7 @@ package game;
 
 import game.datacollections.AbilityMinionData;
 import status.Status;
+import status.StatusCode;
 
 public class AbilityMinion extends Minion {
 
@@ -15,5 +16,18 @@ public class AbilityMinion extends Minion {
             return attackStatus;
         }
         return ((AbilityMinionData)data).getAbility().UseAbility(this);
+    }
+
+    @Override
+    Status cast(Minion minion) {
+        if (getHasAttacked()) {
+            return new Status(StatusCode.kAborted, "Attacker card has already attacked this turn.");
+        }
+        if (getIsFrozen()) {
+            return new Status(StatusCode.kAborted, "Attacker card is frozen.");
+        }
+        minion.OnAttacked(attackDamage);
+        hasAttacked = true;
+        return Status.ok();
     }
 }
